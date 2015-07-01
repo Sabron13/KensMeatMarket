856378cc1dc78cec037db71e36c1889f6020a450
Sabron13 <kurt.schnelker@gmail.com>
2015-06-30 21:28:48 -0400

    adds facebook SDK

--- public/js/application.js
+++ public/js/application.js
@@ -14,5 +14,23 @@ $(document).ready(function() {
       position:mapOptions.center,
     });
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+    marker.setMap(map);
+  }
+  google.maps.event.addDomListener(window, 'load', initialize);
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '661197394013848',
+      xfbml      : true,
+      version    : 'v2.3'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
 });
