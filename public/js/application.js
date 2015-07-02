0ce3469084ff783feccdb49c62cb51cc408bf47a
Sabron13 <kurt.schnelker@gmail.com>
2015-07-02 15:41:21 -0400

    takes out console.log from google API call, and removes facebook sdk from application.js

--- public/js/application.js
+++ public/js/application.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
   //google map api
   function initialize() {
     var mapCanvas = document.getElementById('map-canvas');
-    console.log(mapCanvas);
     var mapOptions = {
       center: new google.maps.LatLng(41.070046, -85.017082),
       zoom: 13,
@@ -18,19 +17,6 @@ $(document).ready(function() {
   }
   google.maps.event.addDomListener(window, 'load', initialize);
 
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '661197394013848',
-      xfbml      : true,
-      version    : 'v2.3'
-    });
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
+  //facebook sdk
+  
 });
