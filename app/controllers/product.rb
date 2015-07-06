ac5a191f2eefd0998f1a292092ae0145ab4f880d
Sabron13 <kurt.schnelker@gmail.com>
2015-07-06 16:52:11 -0400

    adds category and description routes to reorder them accordingly

--- app/controllers/product.rb
+++ app/controllers/product.rb
@@ -2,3 +2,17 @@ get '/products/index' do
   @products = Product.find_by_sql("SELECT * FROM products ORDER BY products.name")
   erb :'/products/index'
 end
+
+get '/products/category' do
+  if request.xhr?
+    @products = Product.find_by_sql("SELECT * FROM products ORDER BY products.category")
+    erb :'/products/index', layout: false
+  else
+    p "expect the unexpected"
+  end
+end
+
+get '/products/description' do
+  @products = Product.find_by_sql("SELECT * FROM products ORDER BY products.description")
+  erb :'/products/index'
+end
