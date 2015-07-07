c3612b23ec6b63966165e07693e0bbdcd534587d
Sabron13 <kurt.schnelker@gmail.com>
2015-07-07 15:09:30 -0400

    puts in handler for ajax call

--- app/controllers/product.rb
+++ app/controllers/product.rb
@@ -1,6 +1,10 @@
 get '/products/index' do
   @products = Product.find_by_sql("SELECT * FROM products ORDER BY products.name")
-  erb :'/products/index'
+  if request.xhr?
+    erb :'/products/index', layout: false
+  else
+    erb :'/products/index'
+  end
 end
 
 get '/products/category' do
