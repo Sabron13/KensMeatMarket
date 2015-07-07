f368e460e09833070a29a8c8cf69165bfcd71e5f
Sabron13 <kurt.schnelker@gmail.com>
2015-07-07 15:10:26 -0400

    moves class variable out of conditional statement

--- app/controllers/product.rb
+++ app/controllers/product.rb
@@ -8,8 +8,8 @@ get '/products/index' do
 end
 
 get '/products/category' do
+  @products = Product.find_by_sql("SELECT * FROM products ORDER BY products.category")
   if request.xhr?
-    @products = Product.find_by_sql("SELECT * FROM products ORDER BY products.category")
     erb :'/products/index', layout: false
   else
     p "expect the unexpected"
@@ -17,8 +17,8 @@ get '/products/category' do
 end
 
 get '/products/description' do
+  @products = Product.find_by_sql("SELECT * FROM products ORDER BY products.description")
   if request.xhr?
-    @products = Product.find_by_sql("SELECT * FROM products ORDER BY products.description")
     erb :'/products/index', layout: false
   else
     p "expect the unexpected"
