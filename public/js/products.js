08e4e1d943b56fdbe60b50798a3ee430d3a2a1d3
Sabron13 <kurt.schnelker@gmail.com>
2015-10-09 14:27:25 -0400

    deletes ajax call for description ids, adds a function for fadeing a description div in and out on hover.

--- public/js/products.js
+++ public/js/products.js
@@ -1,15 +1,20 @@
-$('#category').click(function(event) {
-  event.preventDefault();
-  var $target = $(event.target);
-  $.ajax({
-    url: $target.attr("href"),
-    type: 'GET'
-  }).done(function(response){
-    $("#product-wrapper").html(response);
-  });
+$(".small-4.columns").mouseenter(function(){
+    var image= $(this).find('img'),
+        description = $(this).find('div');
+
+    description.width(image.width());
+    description.height(image.height());
+    description.fadeIn();
+}).mouseleave(function(){
+     var image= $(this).find('img'),
+        description = $(this).find('div');
+    
+    description.width(image.width());
+    description.height(image.height());
+    description.fadeOut();
 });
 
-$('#description').click(function(event) {
+$('#category').click(function(event) {
   event.preventDefault();
   var $target = $(event.target);
   $.ajax({
