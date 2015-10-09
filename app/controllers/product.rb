b06856b3afcc82c020acfb3d54f3d1a627b61584
Sabron13 <kurt.schnelker@gmail.com>
2015-10-09 14:23:13 -0400

    removes unused route.

--- app/controllers/product.rb
+++ app/controllers/product.rb
@@ -11,8 +11,3 @@ get '/products/category' do
   @products = Product.find_by_sql("SELECT * FROM products ORDER BY products.category")
   erb :'/products/index', layout: false if request.xhr?
 end
-
-get '/products/description' do
-  @products = Product.find_by_sql("SELECT * FROM products ORDER BY products.description")
-  erb :'/products/index', layout: false if request.xhr?
-end
